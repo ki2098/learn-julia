cdea95fe3a2b690f10cafd09e8b694c83a7036c4
ki2098 <1336670149@qq.com>
2025-12-03 14:39:13 +0900

    backup

--- mpi/test-run.sh
+++ mpi/test-run.sh
@@ -6,8 +6,8 @@
 #PJM -j
 #PJM -o test-run.log
 
-module load cuda julia
+module load julia
 
-julia --project -e 'using CUDA; CUDA.set_runtime_version!(v"12.2")'
+# julia --project -e 'using CUDA; CUDA.set_runtime_version!(v"12.2")'
 
 ~/.julia/bin/mpiexecjl -n 8 julia --project foo.jl
