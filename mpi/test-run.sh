7d703baa8b9364c424290a171be3ce0f588b161f
ki2098 <1336670149@qq.com>
2025-12-03 10:25:09 +0900

    backup

--- mpi/test-run.sh
+++ mpi/test-run.sh
@@ -2,6 +2,7 @@
 
 #PJM -L rscgrp=b-batch
 #PJM -L node=1
+#PJM --mpi proc=4
 #PJM -L elapse=00:30:00
 #PJM -j
 #PJM -o test-run.log
@@ -10,4 +11,4 @@ module load nvidia nvompi julia
 
 julia cuda-hpc-setup.jl 12.2
 
-mpiexec -n 8 julia --project foo.jl
+mpiexec -n $PJM_MPI_PROCS -map-by ppr:$PJM_PROC_BY_NODE:node julia --project foo.jl
